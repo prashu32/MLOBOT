8d7f0d8c538299017f41846b4b67f246dab9c8ab
PROFESSOR <75575809+prashu32@users.noreply.github.com>
2020-12-07 07:22:11 +0530

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,12 +1,12 @@
 import glob
-from telebot import bot
+from Mlobot import bot
 from sys import argv
 from telethon import TelegramClient
-from telebot.telebotConfig import Var
-from telebot.utils import load_module, start_mybot, load_pmbot
+from mlobot.mlobotConfig import Var
+from mlobot.utils import load_module, start_mybot, load_pmbot
 from pathlib import Path
 import telethon.utils
-from telebot import CMD_HNDLR
+from mlobot import CMD_HNDLR
 
 TELE = Var.PRIVATE_GROUP_ID
 BOTNAME = Var.TG_BOT_USER_NAME_BF_HER
@@ -21,7 +21,7 @@ async def add_bot(bot_token):
 
 async def startup_log_all_done():
     try:
-        await bot.send_message(TELE, f"**TeleBot has been deployed.\nSend** `{CMD_HNDLR}alive` **to see if the bot is working.\n\nAdd** @{BOTNAME} **to this group and make it admin for enabling all the features of TeleBot**")
+        await bot.send_message(TELE, f"**MloBot has been deployed successfully surr.\nSend** `{CMD_HNDLR}alive` **to see if the bot is working.\n\nAdd** @{BOTNAME} **to this group and make it admin for enabling all the features of MloBot**")
     except BaseException:
         print("Either PRIVATE_GROUP_ID is wrong or you have left the group.")
 
@@ -38,13 +38,13 @@ else:
             api_hash=Var.API_HASH
         ).start(bot_token=Var.TG_BOT_TOKEN_BF_HER)
         print("Initialisation finished, no errors")
-        print("Starting Userbot")
+        print("Starting Userbot sirr")
         bot.loop.run_until_complete(add_bot(Var.TG_BOT_USER_NAME_BF_HER))
-        print("Startup Completed")
+        print("Startup Completed sir")
     else:
         bot.start()
 
-path = 'telebot/plugins/*.py'
+path = 'mlobot/plugins/*.py'
 files = glob.glob(path)
 for name in files:
     with open(name) as f:
@@ -52,10 +52,10 @@ for name in files:
         shortname = path1.stem
         load_module(shortname.replace(".py", ""))
 
-print("TeleBot has been deployed! ")
+print("MLOBOT has been deployed! ")
 
 print("Setting up TGBot")
-path = "telebot/plugins/mybot/*.py"
+path = "mlobot/plugins/mybot/*.py"
 files = glob.glob(path)
 for name in files:
     with open(name) as f:
@@ -64,7 +64,7 @@ for name in files:
         start_mybot(shortname.replace(".py", ""))
 
 if LOAD_MYBOT == "True":
-    path = "telebot/plugins/mybot/pmbot/*.py"
+    path = "mlobot/plugins/mybot/pmbot/*.py"
     files = glob.glob(path)
     for name in files:
         with open(name) as f:
@@ -74,7 +74,7 @@ if LOAD_MYBOT == "True":
     print("TGBot set up completely!")
 
 print("TGBot set up - Level - Basic")
-print("TeleBot has been fully deployed! Do Visit @TeleBotSupport")
+print("MloBot has been fully deployed sir! Do Visit @MLO_USERBOT")
 bot.loop.run_until_complete(startup_log_all_done())
 
 if len(argv) not in (1, 3, 4):
