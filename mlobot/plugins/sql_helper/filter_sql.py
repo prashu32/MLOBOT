4da47485d37ba2ebc402acb0a4824fa62979c668
PROFESSOR <75575809+prashu32@users.noreply.github.com>
2020-12-10 08:53:11 +0530

    Update and rename sql_helper/filter_sql.py to mlobot/plugins/sql_helper/filter_sql.py

--- mlobot/plugins/sql_helper/filter_sql.py
+++ mlobot/plugins/sql_helper/filter_sql.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, UnicodeText, LargeBinary, Numeric, String
-from telebot.plugins.sql_helper import SESSION, BASE
+from mlobot.plugins.sql_helper import SESSION, BASE
 
 
 class Filters(BASE):
