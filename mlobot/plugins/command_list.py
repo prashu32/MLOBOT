44490177181a42502fdc555667aee97a4f87d422
PROFESSOR <75575809+prashu32@users.noreply.github.com>
2020-12-11 09:09:10 +0530

    Update and rename command_list.py to mlobot/plugins/command_list.py

--- mlobot/plugins/command_list.py
+++ mlobot/plugins/command_list.py
@@ -1,19 +1,19 @@
-# Join @TeleBotHelp for custom plugins
+# Join @Mlo_Userbot for custom plugins
 
 import asyncio
 
 import requests
 
-from telebot import CMD_HELP
+from mlobot import CMD_HELP
 
 
-@telebot.on(admin_cmd(pattern="cmds", outgoing=True))
-@telebot.on(sudo_cmd(pattern="cmds", allow_sudo=True))
+@mlobot.on(admin_cmd(pattern="cmds", outgoing=True))
+@mlobot.on(sudo_cmd(pattern="cmds", allow_sudo=True))
 async def install(event):
     if event.fwd_from:
         return
     tele = await eor(event, "`Searching for all plugins...`")
-    cmd = "ls telebot/plugins"
+    cmd = "ls mlobot/plugins"
     process = await asyncio.create_subprocess_shell(
         cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
     )
@@ -23,14 +23,14 @@ async def install(event):
     o = "\n".join(_o)
     OUTPUT = (
         OUTPUT
-    ) = f"Here is the list of plugins found in 'master' branch of TeleBot.\n{o}\n\nUse .help <cmd_name> to learn how a paticular plugin works.\nConsider joining @TeleBotSupport for help!"
+    ) = f"Here is the list of plugins found in 'master' branch of MloBot.\n{o}\n\nUse .help <cmd_name> to learn how a paticular plugin works.\nConsider joining @Mlo_Userbot for help!"
     await tele.edit("`Plugins extracted, pasting it...`")
     message = OUTPUT
     url = "https://del.dog/documents"
     r = requests.post(url, data=message.encode("UTF-8")).json()
     url = f"https://del.dog/{r['key']}"
     await tele.edit(
-        f"`All plugins available in` **TeleBot** `can be found` [here]({url})!!"
+        f"`All plugins available in` **MloBot** `can be found` [here]({url})!!"
     )
 
 
