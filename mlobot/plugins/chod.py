15a8ddbd764e854bc63c03dd712ac563228c461d
PROFESSOR <75575809+prashu32@users.noreply.github.com>
2020-12-12 15:31:51 +0530

    Update chod.py

--- mlobot/plugins/chod.py
+++ mlobot/plugins/chod.py
@@ -14,10 +14,10 @@ import asyncio
 
 from telethon import events
 
-from telebot import CMD_HELP
+from mlobot import CMD_HELP
 
 
-@telebot.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+@mlobot.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
 async def _(event):
 
     if event.fwd_from:
@@ -55,7 +55,7 @@ async def _(event):
             await event.edit(animation_chars[i % 11])
 
 
-@telebot.on(events.NewMessage(pattern=r"(.*)", outgoing=True))
+@mlobot.on(events.NewMessage(pattern=r"(.*)", outgoing=True))
 async def _(event):
 
     if event.fwd_from:
@@ -93,7 +93,7 @@ async def _(event):
             await event.edit(animation_chars[i % 11])
 
 
-@telebot.on(events.NewMessage(pattern=r"(.*)", outgoing=True))
+@mlobot.on(events.NewMessage(pattern=r"(.*)", outgoing=True))
 async def _(event):
 
     if event.fwd_from:
