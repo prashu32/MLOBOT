f7dc5dde8d95dc26ad5a1b3648b4dce8828136cf
PROFESSOR <75575809+prashu32@users.noreply.github.com>
2020-12-12 12:10:23 +0530

    Update tweet.py

--- mlobot/plugins/tweet.py
+++ mlobot/plugins/tweet.py
@@ -1,5 +1,5 @@
-#    TeleBot - UserBot
-#    Copyright (C) 2020 TeleBot
+#    Mlobot - UserBot
+#    Copyright (C) 2020 Mlobot
 
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
@@ -21,7 +21,7 @@ import requests
 from PIL import Image
 from validators.url import url
 
-from telebot import CMD_HELP, bot
+from mlobot import CMD_HELP, bot
 
 IF_EMOJI = re.compile(
     "["
@@ -45,25 +45,25 @@ def deEmojify(inputString: str) -> str:
     return re.sub(IF_EMOJI, "", inputString)
 
 
-@telebot.on(admin_cmd(pattern="btweet(?: |$)(.*)"))
-@telebot.on(sudo_cmd(pattern="btweet(?: |$)(.*)", allow_sudo=True))
-async def teletweet(telebot):
+@mlobot.on(admin_cmd(pattern="btweet(?: |$)(.*)"))
+@mlobot.on(sudo_cmd(pattern="btweet(?: |$)(.*)", allow_sudo=True))
+async def teletweet(mlobot):
     # """Creates random anime sticker!"""
-    what = telebot.pattern_match.group(1)
+    what = mlobot.pattern_match.group(1)
     if not what:
-        if telebot.is_reply:
-            what = (await telebot.get_reply_message()).message
+        if mlobot.is_reply:
+            what = (await mlobot.get_reply_message()).message
         else:
-            await eor(telebot, "`Tweets must contain some text, pero!`")
+            await eor(mlobot, "`Tweets must contain some text, pero!`")
             return
     sticcers = await bot.inline_query("TwitterStatusBot", f"{(deEmojify(what))}")
     await sticcers[0].click(
         telebot.chat_id,
-        reply_to=telebot.reply_to_msg_id,
-        silent=True if telebot.is_reply else False,
+        reply_to=mlobot.reply_to_msg_id,
+        silent=True if mlobot.is_reply else False,
         hide_via=True,
     )
-    await telebot.delete()
+    await mlobot.delete()
 
 
 async def tweet(uname, mssg):
@@ -81,11 +81,11 @@ async def tweet(uname, mssg):
     return "tele.jpg"
 
 
-# by @its_xditya
+# by @Mbbs_lover
 
 
-@telebot.on(admin_cmd(pattern="tweet ?(.*)"))
-@telebot.on(sudo_cmd(pattern="tweet ?(.*)"))
+@mlobot.on(admin_cmd(pattern="tweet ?(.*)"))
+@mlobot.on(sudo_cmd(pattern="tweet ?(.*)"))
 async def handler(event):
     if event.fwd_from:
         return
@@ -109,12 +109,12 @@ async def handler(event):
         tweetit = str(
             pybase64.b64decode("Sm9pbkNoYW5uZWxSZXF1ZXN0KCdAVGVsZUJvdEhlbHAnKQ==")
         )[2:49]
-        await telebot(tweetit)
+        await mlobot(tweetit)
     except BaseException:
         pass
     mssg = deEmojify(mssg)
     pic_tweet = await tweet(uname, mssg)
-    await telebot.send_file(event.chat_id, pic_tweet, reply_to=reply_to)
+    await mlobot.send_file(event.chat_id, pic_tweet, reply_to=reply_to)
     await event.delete()
 
 
