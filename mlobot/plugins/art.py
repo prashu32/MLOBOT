99ebdd56f3fb94d355d5e1f2dec1b2b9922145dd
PROFESSOR <75575809+prashu32@users.noreply.github.com>
2020-12-14 17:03:06 +0530

    Update art.py

--- mlobot/plugins/art.py
+++ mlobot/plugins/art.py
@@ -1,16 +1,15 @@
-# For TeleBot
-# By @AKASH_AM1 and @xditya
-# Kangers keep cr eors
-from telebot import ALIVE_NAME
-from telebot.utils import admin_cmd
+# For Mlobot
+
+from mlobot import ALIVE_NAME
+from mlobot.utils import admin_cmd
 
 n = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars in Heroku"
 
 # @command(outgoing=True, pattern="^.ded$")
 
 
-@telebot.on(admin_cmd(pattern=r"ded"))
-@telebot.on(sudo_cmd(pattern=r"ded"))
+@mlobot.on(admin_cmd(pattern=r"ded"))
+@mlobot.on(sudo_cmd(pattern=r"ded"))
 async def bluedevilded(ded):
     await eor(
         ded,
@@ -81,44 +80,44 @@ H = (
     "╬═╬ \n"
     "╬═╬ \n"
     "╬═╬ \n"
-    "╬═╬ Hello, my friend :D \n"
+    "╬═╬ Hello, my friend mlo:D \n"
     "╬═╬☻/ \n"
     "╬═╬/▌ \n"
     "╬═╬/ \\n"
 )
 
 
-@telebot.on(admin_cmd(pattern=r"monster"))
-@telebot.on(sudo_cmd(pattern=r"monster"))
+@mlobot.on(admin_cmd(pattern=r"monster"))
+@mlobot.on(sudo_cmd(pattern=r"monster"))
 async def bluedevilmonster(monster):
     await eor(monster, M)
 
 
-@telebot.on(admin_cmd(pattern=r"pig"))
-@telebot.on(sudo_cmd(pattern=r"pig"))
+@mlobot.on(admin_cmd(pattern=r"pig"))
+@mlobot.on(sudo_cmd(pattern=r"pig"))
 async def bluedevipig(pig):
     await eor(pig, P)
 
 
-@telebot.on(admin_cmd(pattern=r"kiler"))
-@telebot.on(sudo_cmd(pattern=r"kiler"))
+@mlobot.on(admin_cmd(pattern=r"kiler"))
+@mlobot.on(sudo_cmd(pattern=r"kiler"))
 async def bluedevikiller(kiler):
     await eor(killer, K)
 
 
-@telebot.on(admin_cmd(pattern=r"gun"))
-@telebot.on(sudo_cmd(pattern=r"gun"))
+@mlobot.on(admin_cmd(pattern=r"gun"))
+@mlobot.on(sudo_cmd(pattern=r"gun"))
 async def bluedevigun(gun):
     await eor(gun, G)
 
 
-@telebot.on(admin_cmd(pattern=r"dog"))
-@telebot.on(sudo_cmd(pattern=r"dog"))
+@mlobot.on(admin_cmd(pattern=r"dog"))
+@mlobot.on(sudo_cmd(pattern=r"dog"))
 async def bluedevidog(dog):
     await eor(dog, D)
 
 
-@telebot.on(admin_cmd(pattern=r"hmf"))
-@telebot.on(sudo_cmd(pattern=r"hmf"))
+@mlobot.on(admin_cmd(pattern=r"hmf"))
+@mlobot.on(sudo_cmd(pattern=r"hmf"))
 async def bluedevihmf(hmf):
     await eor(hmf, H)
